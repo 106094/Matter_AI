60d0785b5843358ca44e25339c5fdcb8561f2e39
Allion106094 <allion106094@gmail.com>
2024-09-26 16:32:59 +0800

    update

--- cmdcollecting_tool/Matter_getchiptool.ps1
+++ cmdcollecting_tool/Matter_getchiptool.ps1
@@ -109,13 +109,13 @@ for($i=$Indexfirst;$i -le $Indexlast;$i++){
  $thisneed=0
  $sheetname=$($worksheetNames[$i])
  ($sheetname.split("(").split(")").split(","))|Where-Object{
-  if($_ -in $filteredsheets){
+  if($_.trim() -in $filteredsheets){
    $thisneed=1
   } 
  }
  #$sheetname
  if($thisneed){
- #$sheetname
+ $sheetname
  $sheetdate= Import-Excel $excelfull -WorksheetName $sheetname -NoHeader
  $worksheet = (Open-ExcelPackage -path $excelfull).Workbook.WorkSheets[$sheetname]
  $colproperty = ($sheetdate[0] | Get-Member -MemberType NoteProperty).name
