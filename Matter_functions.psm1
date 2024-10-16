5d66e4694dd579b31cf8f0af43b053e03daac6dc
Allion106094 <allion106094@gmail.com>
2024-10-16 16:35:02 +0800

    add endpoint2

--- Matter_functions.psm1
+++ Matter_functions.psm1
@@ -116,7 +116,8 @@ if(get-process putty){
     if ($manual){
         $endpid0=((get-content C:\Matter_AI\settings\config_linux.txt | Select-String "endpoint0"|out-string).split(":"))[-1].trim()
         $endpid1=((get-content C:\Matter_AI\settings\config_linux.txt | Select-String "endpoint1"|out-string).split(":"))[-1].trim()
-        if($endpid0 -ne 0 -or $endpid1 -ne 1){      
+        $endpid2=((get-content C:\Matter_AI\settings\config_linux.txt | Select-String "endpoint2"|out-string).split(":"))[-1].trim()
+        if($endpid0 -ne 0 -or $endpid1 -ne 1 -or $endpid2 -ne 2){      
         $eplists=import-csv "C:\Matter_AI\settings\chip-tool_clustercmd - endpoint_list.csv"
         $splitcmd=($cmdline.replace("./chip-tool ","")).split(" ")|where-object{$_.Length -gt 0}
         $endpiont=($eplists|Where-Object{$_.name -eq $splitcmd[0] -and $_.command -eq $splitcmd[1] -and $_.attribute -eq $splitcmd[2]}).endpoint
@@ -133,9 +134,12 @@ if(get-process putty){
             if($endpid0 -ne 0 -and $matched -eq 0){
              $cmdline = $cmdline.Substring(0, $numberIndex) + $endpid0 + $cmdline.Substring($numberIndex + $numberLength)   
             }          
-            if($endpid1 -ne 0 -and $matched -eq 1){
+            if($endpid1 -ne 1 -and $matched -eq 1){
              $cmdline = $cmdline.Substring(0, $numberIndex) + $endpid1 + $cmdline.Substring($numberIndex + $numberLength)   
             } 
+            if($endpid2 -ne 2 -and $matched -eq 2){
+                $cmdline = $cmdline.Substring(0, $numberIndex) + $endpid2 + $cmdline.Substring($numberIndex + $numberLength)   
+               } 
          }
         }
        }
