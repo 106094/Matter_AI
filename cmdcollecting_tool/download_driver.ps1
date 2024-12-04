944ce4a1d7b12f16482191957afd543ae75bb292
LazyFinger <97865400+106094@users.noreply.github.com>
2024-12-04 14:31:14 +0800

    Create download_driver.ps1

---
+++ cmdcollecting_tool/download_driver.ps1
@@ -0,0 +1,12 @@
+$version=Get-ItemPropertyValue -Path 'HKCU:\SOFTWARE\Microsoft\Edge\BLBeacon' -Name "version"
+$drivernow=(get-item C:\Matter_AI\cmdcollecting_tool\tool\msedgedriver.exe).VersionInfo.fileversion
+if($drivernow -ne $version){
+    $downloadurl = "https://msedgedriver.azureedge.net/" +  $version + "/edgedriver_win64.zip"
+            
+    $outputPath = "$env:userprofile\downloads\edgedriver.zip"
+    Invoke-WebRequest -Uri $downloadurl -OutFile $outputPath
+    Expand-Archive $outputPath -destinationpath C:\Matter_AI\cmdcollecting_tool\tool\ -Force
+
+    remove-item $outputPath -force
+
+}
